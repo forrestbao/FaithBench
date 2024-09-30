f2551ec94908878a369d796171c01d91887e3700
Forrest Bao <forrest.bao@gmai.com>
2024-09-30 18:50:35 -0500

    update pilot data

--- assign/pilot.py
+++ assign/pilot.py
@@ -5,10 +5,14 @@ df = pd.read_json('examples_to_annotate.jsonl', lines=True)
 
 N = 10 
 
-# BGE embedding 
+BGE embedding 
 embed_in_bge_small = df.sample(n=N)
-embed_in_bge_small.to_json('pilot_bge_small.jsonl', orient='records', lines=True)
+embed_in_bge_small.to_json('pilot_data/pilot_bge_small.jsonl', orient='records', lines=True)
 
 # openai small embedding
 embed_in_openai_small = df.sample(n=N)
-embed_in_openai_small.to_json('pilot_openai_small.jsonl', orient='records', lines=True)
+embed_in_openai_small.to_json('pilot_data/pilot_openai_small.jsonl', orient='records', lines=True)
+
+# all-mpnet-base-v2 embedding
+embed_in_openai_small = df.sample(n=N)
+embed_in_openai_small.to_json('pilot_data/pilot_all-mpnet_small.jsonl', orient='records', lines=True)
