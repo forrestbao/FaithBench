eb61428c670fbbf445dfa7d7c57ca9070ee45aa8
Amin Ahmad <amin@zir-ai.com>
2024-10-11 20:09:50 -0700

    Fixed directory location to absolute path

--- assign/batch_5_src_no_sports/fetch_db.sh
+++ assign/batch_5_src_no_sports/fetch_db.sh
@@ -1,2 +1,2 @@
-gcloud compute scp leaderboard-annotation:~/mercury/batch_5_src_no_sports/virgin_data/*.sqlite results  --project "ml-training-382218" --zone "us-central1-c"
+gcloud compute scp leaderboard-annotation:/home/forrest/mercury/batch_5_src_no_sports/virgin_data/*.sqlite results  --project "ml-training-382218" --zone "us-central1-c"
 # gcloud compute scp leaderboard-annotation:~/mercury/batch_5_src_no_sports/virgin_data/batch_3.sqlite results  --project "ml-training-382218" --zone "us-central1-c"
